5fe6cb3e4aa3fc39a517500660a55e39c72d9692
DeekshaReddyM <69183696+DeekshaReddyM@users.noreply.github.com>
2020-10-07 22:08:01 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,32 @@
+ class Customer
+{
+  String name;
+  String address;
+  String name()
+ {
+   name = Rakshitha;
+ }
+  String address()
+ {
+   address = Bangalore;
+ }
+}
+  class Customers
+{
+  public static void main(String args[])
+ {
+   Customer C = new Customer();
+   String str,str1 = "Anu"
+   Scanner S = new Scanner(System in);
+   System.out.println("Enter the string:");
+   str = S.nextline();
+   if(str.equals(str1))
+ {
+   System.ot.println("Customer details:");
+   System.out.println("Name"+ "c.name"+ "address"+ "c.address");
+ }
+   else
+ {
+   System.out.println("Customer not found");
+ }
+} 
